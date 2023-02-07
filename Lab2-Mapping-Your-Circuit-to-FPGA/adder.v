3a60059a4069f95a978365484ee25bc9b84ffd28
Blue-Silence <blue_silence@outlook.com>
2023-02-07 15:17:25 +0800

    Bug fix.

--- Lab2-Mapping-Your-Circuit-to-FPGA/adder.v
+++ Lab2-Mapping-Your-Circuit-to-FPGA/adder.v
@@ -9,10 +9,10 @@ module FullAdder(input a, input b, input ci, output s, output co);
 endmodule
 
 
-module FourBitAdder(input [3:0] a, input [3:0] b, output [4:0] s);
+module FourBitFullAdder(input [3:0] a, input [3:0] b, output [4:0] s);
    wire c01, c12, c23;
-   adder a0 (a[0], b[0], 1'b0, s[0], c01);
-   adder a1 (a[1], b[1], c01, s[1], c12);
-   adder a2 (a[2], b[2], c12, s[2], c23);
-   adder a3 (a[3], b[3], c23, s[3], s[4]);
+   FullAdder a0 (a[0], b[0], 1'b0, s[0], c01);
+   FullAdder a1 (a[1], b[1], c01, s[1], c12);
+   FullAdder a2 (a[2], b[2], c12, s[2], c23);
+   FullAdder a3 (a[3], b[3], c23, s[3], s[4]);
 endmodule
